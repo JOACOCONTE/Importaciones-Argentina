55e82f5c108304f10e6b930101d2db1bb33f830e
JOACOCONTE <j.conte19@gmail.com>
2025-01-17 18:54:31 -0300

    V0.0.1
    Beta 1

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Contact2 } from "./components/Contacts2"
+import { Footer } from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Contact2 />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
